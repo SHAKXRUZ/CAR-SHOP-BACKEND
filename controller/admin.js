9e0056595f9dafc3737479c10f8c0615b5f562fe
shaxruz25 <shaxruzismoilov250@gmail.com>
2023-06-05 12:18:58 +0500

    admin panel users pages full

--- controller/admin.js
+++ controller/admin.js
@@ -9,8 +9,6 @@ const adminLogin = async (req, res) => {
     const { email, password } = req.body;
     const { token } = req.headers;
 
-    console.log(token);
-
     let emailValidation =
       email.trim() === ""
         ? res.status(400).send({ msg: "Email kiriting?" })
@@ -27,7 +25,10 @@ const adminLogin = async (req, res) => {
       where: { email: userVerify.email },
     });
 
-    if (adminUserObj.role !== process.env.ROLE) {
+    if (
+      adminUserObj.role !== process.env.ROLE &&
+      adminUserObj.role !== process.env.BIG_ADMIN
+    ) {
       return res.status(401).send({ msg: "You are not an admin?" });
     }
 
@@ -72,6 +73,159 @@ const adminLogin = async (req, res) => {
   }
 };
 
+const adminUserUpdate = async (req, res) => {
+  try {
+    const { id, username, email, password, role, user_images } = req.body;
+    const { admin_token } = req.headers;
+
+    let tokenVerify = jwt.verify(admin_token, process.env.SECRET_KEY);
+
+    let usernameValidation = username.trim().toLowerCase();
+    let emailValidation = email.trim().toLowerCase();
+    let passwordValidation = password.trim().toLowerCase();
+    let roleValidation = role.trim().toLowerCase();
+
+    let usersObj = await Users.findOne({
+      where: { id: id },
+    });
+
+    if (
+      !usernameValidation &&
+      !emailValidation &&
+      !passwordValidation &&
+      !roleValidation &&
+      !user_images
+    ) {
+      return res.status(401).send({ msg: "You didn't edit anything?" });
+    }
+
+    if (tokenVerify.role === process.env.ROLE && roleValidation) {
+      return res.status(401).send({ msg: "You can't change user roles?" });
+    }
+
+    if (
+      tokenVerify.role === process.env.BIG_ADMIN &&
+      roleValidation &&
+      tokenVerify.id === usersObj.id
+    ) {
+      return res.status(401).send({ msg: "Can't you change your role?" });
+    }
+
+    if (
+      tokenVerify.role === process.env.ROLE &&
+      usersObj.role === process.env.BIG_ADMIN
+    ) {
+      return res
+        .status(401)
+        .send({ msg: "You can't change the details of a senior admin?" });
+    }
+
+    if (
+      usersObj.id !== tokenVerify.id &&
+      tokenVerify.role === process.env.ROLE &&
+      usersObj.role === process.env.ROLE
+    ) {
+      return res
+        .status(401)
+        .send({ msg: "You can't change the data of other admins?" });
+    }
+
+    if (
+      (usersObj.id === tokenVerify.id &&
+        tokenVerify.role === process.env.ROLE) ||
+      usersObj.role === "user"
+    ) {
+      await Users.update(
+        {
+          username: usernameValidation ? usernameValidation : usersObj.username,
+          email: emailValidation ? emailValidation : usersObj.email,
+          password: passwordValidation ? passwordValidation : usersObj.password,
+          user_images: user_images ? user_images : usersObj.user_images,
+        },
+        {
+          returning: true,
+          where: {
+            id,
+          },
+        }
+      );
+    }
+
+    await Users.update(
+      {
+        username: usernameValidation ? usernameValidation : usersObj.username,
+        email: emailValidation ? emailValidation : usersObj.email,
+        password: passwordValidation ? passwordValidation : usersObj.password,
+        user_images: user_images ? user_images : usersObj.user_images,
+        role: roleValidation ? roleValidation : usersObj.role,
+      },
+      {
+        returning: true,
+        where: {
+          id,
+        },
+      }
+    );
+
+    return res.status(201).send({ msg: "User update!" });
+  } catch {
+    res.send({ msg: "Error" });
+  }
+};
+
+const adminUsersDelete = async (req, res) => {
+  try {
+    const { id } = req.body;
+    const { admin_token } = req.headers;
+
+    let tokenVerify = jwt.verify(admin_token, process.env.SECRET_KEY);
+
+    let usersObj = await Users.findOne({
+      where: { id: id },
+    });
+
+    let tokenVerifyRole = tokenVerify.role;
+    let usersObjRole = usersObj.role;
+
+    if (usersObjRole === process.env.BIG_ADMIN) {
+      return res.status(401).send({ msg: "You can't remove senior admin?" });
+    } else if (
+      tokenVerifyRole !== process.env.BIG_ADMIN &&
+      usersObjRole === process.env.ROLE
+    ) {
+      return res.status(401).send({ msg: "You can't delete other admins?" });
+    } else if (
+      tokenVerifyRole === process.env.ROLE &&
+      usersObjRole === "user"
+    ) {
+      await Users.destroy({
+        returning: true,
+        plain: true,
+        where: {
+          id,
+        },
+      });
+    } else if (
+      tokenVerifyRole === process.env.BIG_ADMIN &&
+      usersObjRole !== process.env.BIG_ADMIN
+    ) {
+      await Users.destroy({
+        returning: true,
+        plain: true,
+        where: {
+          id,
+        },
+      });
+    }
+
+    return res.status(201).send({ msg: "User deleted!" });
+  } catch {
+    res.send({ msg: "Error" });
+  }
+};
+
 module.exports = {
   adminLogin,
+  adminUserUpdate,
+  adminUsersDelete,
 };
--- router/users_router.js
+++ router/users_router.js
@@ -4,6 +4,7 @@ const {
   userRegistr,
   userLogin,
   getProfelImages,
+  searchUser,
 } = require("../controller/users.js");
 
 const router = Router();
@@ -12,5 +13,6 @@ router.get("/list", getUsers);
 router.post("/registr", userRegistr);
 router.post("/login", userLogin);
 router.get("/profel_images", getProfelImages);
+router.get("/user_search_api", searchUser);
 
 module.exports = router;
